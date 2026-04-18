9356249b001dd31625752d1fb1547ad9e7fbc28b
cevamnelampaplagin <malakoza00@gmail.com>
2026-04-18 03:10:17 +0300

    Add files via upload

---
+++ antidmca0.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+
+    window.anti_dmca_plugin = 'happy end';
+
+    function start() {
+        if (window.anti_dmca_plugin) {
+            return;
+        }
+
+        window.anti_dmca_plugin = true;
+        window.lampa_settings.dcma = [];
+
+        Lampa.Utils.dcma = function () { return undefined };
+        var defaultSource = Lampa.Storage.get('source', 'cub');
+
+        Lampa.Listener.follow('request_secuses', function (event) {
+            if (event.data.blocked) {
+                window.lampa_settings.dcma = [];
+
+                var active = Lampa.Activity.active();
+                active.source = 'tmdb';
+                Lampa.Storage.set('source', 'tmdb', true);
+
+                setTimeout(function () {
+                    setTimeout(function () {
+                        Lampa.Activity.replace(active);
+                        Lampa.Storage.set('source', defaultSource, true);
+                    }, 300);
+                    Lampa.Controller.toggle('content');
+                }, 250);
+            }
+        });
+    }
+
+    if (window.appready) {
+        start();
+    } else {
+        Lampa.Listener.follow('app', function (event) {
+            if (event.type === 'ready') {
+                start();
+            }
+        });
+    }
+})();
