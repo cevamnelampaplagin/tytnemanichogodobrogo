98e46f5081a3457f89a203f66cdbe5d01998f0dc
cevamnelampaplagin <malakoza00@gmail.com>
2026-08-06 00:24:13 +0300

    Update ko3pars++.js

--- ko3pars++.js
+++ ko3pars++.js
@@ -15,7 +15,7 @@
         { base: 'spawpp_ua_59117', shortName: 'Spawnpp', name: 'SpawnppUA (toloka, mazepa, etc.)', url: 'http://spawn.pp.ua:59117', settings: { key: '2', parser_torrent_type: 'jackett' } },
         { base: 'otlampa', shortName: 'vsyake', name: 'Всяке різне (7)', url: 'http://87.120.84.218:9117', settings: { key: '333', parser_torrent_type: 'jackett' } },
         { base: 'jac_red', shortName: 'Jac.red', name: 'Jac.red (11)', url: 'jac.red', settings: { key: '', parser_torrent_type: 'jackett' } },
-        { base: 'jac_black', shortName: 'Jac.black', name: 'Jac.black (?)', url: 'jac.black', settings: { key: '', parser_torrent_type: 'jackett' } },
+        { base: 'jac_black', shortName: 'Jac.black', name: 'Jac.black (13)', url: 'jac.black', settings: { key: '', parser_torrent_type: 'jackett' } },
         { base: 'jac_stull', shortName: 'Jac.Stull', name: 'Jac.stull (12)', url: 'jac.stull.xyz', settings: { key: '1', parser_torrent_type: 'jackett' } },
         { base: 'jacred_stream', shortName: 'Jacred.Stream', name: 'Jacred stream (14)', url: 'jacred.stream', settings: { key: 'pp', parser_torrent_type: 'jackett' } },
         { base: '407_xyz', shortName: '407_xyz', name: '407_xyz (12)', url: '11.307407.xyz', settings: { key: '', parser_torrent_type: 'jackett' } },
